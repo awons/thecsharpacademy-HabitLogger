0c1f271d6d8c5c396e634a6af2ba27cf1daa4664
Aleksander Wons <aleksander.wons@gmail.com>
2024-11-19 21:00:44 +0100

    Add class library and corresponding test projects

--- HabitLoggerApp/Program.cs
+++ HabitLoggerApp/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Data.Sqlite;
 
-Console.WriteLine("Hello, World!");
+using var connection = new SqliteConnection("Data Source = HabitLogger.db");
+
+connection.Open();
+
+var command = connection.CreateCommand();
+command.CommandText = @"CREATE TABLE IF NOT EXISTS habit_logs (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    habit TEXT NOT NULL,
+    quantity INTEGER NOT NULL,
+    habit_date DATE NOT NULL,
+    UNIQUE(habit, quantity)
+)";
+command.ExecuteNonQuery();
