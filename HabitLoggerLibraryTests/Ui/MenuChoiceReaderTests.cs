e0a8c830fdbe1cadd7514be2dcb002be863b1d14
Aleksander Wons <aleksander.wons@gmail.com>
2024-11-28 20:57:56 +0100

    Add Add additional test for menu choice reader to check if it will wait for a valid choice

--- HabitLoggerLibraryTests/Ui/MenuChoiceReaderTests.cs
+++ HabitLoggerLibraryTests/Ui/MenuChoiceReaderTests.cs
@@ -19,6 +19,18 @@ public class MenuChoiceReaderTests
         reader.GetChoice().Should().Be(valueTuple.expectedResult);
     }
 
+    [Test]
+    public void WillKeepOnWaitingForValidChoice()
+    {
+        var consoleWrapper = Substitute.For<IConsoleWrapper>();
+        consoleWrapper.ReadKey().Returns(new ConsoleKeyInfo('a', ConsoleKey.None, false, false, false),
+            new ConsoleKeyInfo('b', ConsoleKey.None, false, false, false),
+            new ConsoleKeyInfo('q', ConsoleKey.None, false, false, false));
+        var reader = new MenuChoiceReader(consoleWrapper);
+
+        reader.GetChoice().Should().Be(MenuChoice.Quit);
+    }
+
     private static (ConsoleKeyInfo, MenuChoice)[] _validCases =
     [
         (new ConsoleKeyInfo('v', ConsoleKey.None, false, false, false), MenuChoice.ViewAllRecords),
