15dc4022a3632f467dd31f432331426ceb5332c4
Aleksander Wons <aleksander.wons@gmail.com>
2024-12-02 18:37:43 +0100

    Make sure app runs in a loop

--- HabitLoggerApp/Application/App.cs
+++ HabitLoggerApp/Application/App.cs
@@ -38,8 +38,6 @@ public sealed class App(DatabaseManagerFactory databaseManagerFactory, IMenuChoi
                 case MenuChoice.EditRecord:
                     break;
             }
-
-            break;
         } while (true);
     }
 }
